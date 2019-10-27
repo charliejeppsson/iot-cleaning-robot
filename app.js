c15a62b39286f6b3aac29fe8deadae59b3a48130
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2019-10-27 14:33:22 +0100

    Set app basic express server

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.post('/tibber-developer-test/enter-path', function(req, res) {
+  console.log('Post endpoint hit!');
+});
+
+var HOST = process.env.HOST || 'localhost';
+var PORT = process.env.PORT || 5000;
+
+app.listen(PORT, HOST, function() {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+});
